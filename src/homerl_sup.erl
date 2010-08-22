b99206dc24434dca16c0a83cfd6c1fa666910065
Nadia <nadiamt@gmail.com>
2010-08-22 19:58:51 +0200

    Added a supervisor callback to start the inets application.

--- src/homerl_sup.erl
+++ src/homerl_sup.erl
@@ -28,18 +28,18 @@ worker(Name) -> server(Name, worker).
 %% @doc Add processes if necessary.
 upgrade() ->
     {ok, {_, Specs}} = init([]),
-
+    
     Old = sets:from_list(
-[Name || {Name, _, _, _} <- supervisor:which_children(?MODULE)]),
+	    [Name || {Name, _, _, _} <- supervisor:which_children(?MODULE)]),
     New = sets:from_list([Name || {Name, _, _, _, _, _} <- Specs]),
     Kill = sets:subtract(Old, New),
-
+    
     sets:fold(fun (Id, ok) ->
-supervisor:terminate_child(?MODULE, Id),
-supervisor:delete_child(?MODULE, Id),
-ok
-end, ok, Kill),
-
+		      supervisor:terminate_child(?MODULE, Id),
+		      supervisor:delete_child(?MODULE, Id),
+		      ok
+	      end, ok, Kill),
+    
     [supervisor:start_child(?MODULE, Spec) || Spec <- Specs],
     ok.
 
@@ -48,4 +48,5 @@ end, ok, Kill),
 init([]) ->
     HomerlServer = worker(homerl_server),
     Dets = worker(dets),
-    {ok, {{one_for_one, 10, 10}, [HomerlServer, Dets]}}.
+    Inets = worker(inets),
+    {ok, {{one_for_one, 10, 10}, [HomerlServer, Dets, Inets]}}.
